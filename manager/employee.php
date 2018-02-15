a8b34840ec678f273f9aed7349913a367f97681d
Babak1990 <36508212+Babak1990@users.noreply.github.com>
2018-02-15 16:49:54 +0100

    Add Employee

--- manager/employee.php
+++ manager/employee.php
@@ -54,7 +54,7 @@
                             <h2>Manage <b>Employee</b></h2>
                         </div>
                             <div class="col-sm-6">
-                                <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Mission</span></a>
+                                <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
                                 <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						
                             </div>
                     </div>
@@ -123,39 +123,52 @@
                 <div class="modal-content">
                     <form>
                         <div class="modal-header">						
-                            <h4 class="modal-title">Add Mission</h4>
+                            <h4 class="modal-title">Add Employee</h4>
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                         </div>
                         <div class="modal-body">	
                             <div class="form-group">
-                                <label>Status</label>
+                                <label>First Name</label>
+                                <input type="text" class="form-control" required>
+                            </div>
+                             <div class="form-group">
+                                <label>Last Name</label>
+                                <input type="text" class="form-control" required>
+                            </div>
+                             <div class="form-group">
+                                <label>Email address</label>
+                                <input type="email" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label>User Name</label>
                                 <input type="text" class="form-control" required>
                             </div>
                             <div class="form-group">
-                                <label>Start Date</label>
-                                <input type="date" class="form-control" required>
+                                <label>Password</label>
+                                <input type="password" class="form-control" required>
                             </div>
                             <div class="form-group">
-                                <label>End Date</label>
-                                <input type="date" class="form-control" required>
+                                <label>Password confirmation</label>
+                                <input type="password" class="form-control" required>
                             </div>
                             <div class="form-group">
-                                <label>Purpose</label>
+                                <label>Address</label>
                                 <input type="text" class="form-control" required>
                             </div>
                             <div class="form-group">
-                                <label for="formEmployeeSelect">Employee</label>
-                                <select class="form-control" id="formEmployeeSelect">
-                                    <option>Name 1</option>
-                                    <option>Name 2</option>
-                                </select>
+                                <label>Date of Birth</label>
+                                <input type="date" class="form-control" required>
                             </div>
                             <div class="form-group">
-                                <label for="formManagerSelect">Manager</label>
-                                <select class="form-control" id="formManagerSelect">
-                                    <option>Name 1</option>
-                                    <option>Name 2</option>
-                                </select>
+                                <label>Phone number</label>
+                                <input type="text" class="form-control" required>
+                            </div>
+                            <label>Type of Employee</label>
+                            <input type="text" class="form-control" required>
+                            
+                        </div>
+                           
+                          
                             </div>					
                         </div>
                         <div class="modal-footer">
